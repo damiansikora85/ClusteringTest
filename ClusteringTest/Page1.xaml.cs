1fdc3171c85b17ee8fc6625dbb4d1effc054f49f
Damian Sikora <dsikora85@gmail.com>
2019-10-15 09:18:45 +0200

    Initial commit

---
+++ ClusteringTest/Page1.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ClusteringTest
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        private int _pointId;
+
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Map_MapClicked(object sender, Xamarin.Forms.GoogleMaps.MapClickedEventArgs e)
+        {
+            Map.Pins.Add(new Xamarin.Forms.GoogleMaps.Pin
+            {
+                Position = new Xamarin.Forms.GoogleMaps.Position(e.Point.Latitude, e.Point.Longitude),
+                Label = $"Pin: {_pointId}"
+            });
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Map.Pins.Clear();
+        }
+    }
+}
